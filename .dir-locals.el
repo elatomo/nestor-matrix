a976ba6b8ea3db6b4fb607dfed1469feca35834c
José Fernández Ramos <jose@navika.io>
2025-11-20 13:10:57 +0100

    Add .dir-locals.el to enable asyncio mode in Python REPL

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables for nestor-matrix
+;;; For more information see (info "(emacs) Directory Variables")
+
+;;; Configures Python REPL to use asyncio mode
+((python-mode . ((python-shell-interpreter-args . "-m asyncio"))))
